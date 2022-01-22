f10764290445465e87c28d51bbaabc15b861e52c
Zach Frey <zach@znfrey.com>
2022-01-22 08:05:51 -0500

    First working test! Contract owner check.

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.4;
 
 contract Vault1 {
+
+    address public owner;
+
+    // Constructor: save owner for later
+    constructor() {
+        owner = msg.sender;
+    }
     
+    function deposit(uint _amount) external pure {
+        return;
+    }
+
+    function withdraw(uint _amount) external pure {
+        return;
+    }
+
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -1,7 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+
 describe("Vault 1", () => {
+	let Vault1, vault1;
+    let owner;
+
 	beforeEach(async () => {
 		Vault1 = await ethers.getContractFactory("Vault1");
 		[owner] = await ethers.getSigners();
@@ -9,5 +13,13 @@ describe("Vault 1", () => {
 		vault1 = await Vault1.deploy();
 
 		await vault1.deployed();
+	});
+
+    describe("Deployment", function () {
+		it("Should set the right owner", async function () {
+			// This test expects the owner variable stored in the contract to be equal
+			// to our Signer's owner.
+			expect(await vault1.owner()).to.equal(owner.address);
+		  });
 	});
 });
